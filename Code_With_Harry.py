e5fe675fd3c85a6f5625ce7cdbf80781f2de7285
harshuop <harshwardhanxyx@gmail.com>
2020-12-13 20:51:09 +0530

    dummy file added

--- Code_With_Harry.py
+++ Code_With_Harry.py
@@ -1,42 +1,11 @@
-mok = set()
-mac = set()
+nums = set()
 
-for mis in range(5):
-    mok.add(mis)
-    mok.add(455)
-for mip in range(8):
-    mac.add(mip)
+x = input('What numbers you choose: ')
 
-print(mok)
-print(mac)
+fok = x.split(' ')
 
-print(mac | mok)
+for fish in fok:
+    nums.add(int(fish))
 
-# tasj = mok.difference(mac)
-# print(tasj)
-
-
-
-
-# set1 = set() 
-# set2 = set() 
-  
-# for i in range(5): 
-#     set1.add(i) 
-  
-# for i in range(3,9): 
-#     set2.add(i) 
-  
-# # Difference of two sets 
-# # using difference() function 
-# set3 = set1.difference(set2) 
-  
-# print(" Difference of two sets using difference() function") 
-# print(set3) 
-  
-# # Difference of two sets 
-# # using '-' operator 
-# set3 = set1 - set2 
-  
-# print("\nDifference of two sets using '-' operator") 
-# print(set3) 
+for charm in nums:
+    print(charm)
