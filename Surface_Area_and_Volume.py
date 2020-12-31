aa4eed063c12fbb682cf7488d7ab564b43fb99ea
harshuop <harshwardhanxyx@gmail.com>
2020-12-31 18:37:39 +0530

    Some minor modifications

--- Surface_Area_and_Volume.py
+++ Surface_Area_and_Volume.py
@@ -1,2 +1,11 @@
 from Code_With_Harry import Square, Rectangle, Parallelogram, Trapezium
 
+shape = input(
+	'**Important Commands**\n # quit "q"\n # help "h"' +
+	'\n  What is your shape: '
+)
+
+if shape.lower() == 'square':
+	input(
+	'\nWhat do you want to find? (ie, area / peri): '
+)
